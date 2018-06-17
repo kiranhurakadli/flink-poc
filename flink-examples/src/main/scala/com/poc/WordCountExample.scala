43b2fb63a96fa21ce6dc4b61248538938813a6ba
kiranhurakadli <kiranhr025@gmail.com>
2018-06-17 17:57:04 +0530

    read from kafka example..

--- flink-examples/src/main/scala/com/poc/WordCountExample.scala
+++ flink-examples/src/main/scala/com/poc/WordCountExample.scala
@@ -1,13 +1,12 @@
 package com.poc
 
-
 import java.util.Properties
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.slf4j.LoggerFactory
 
-object ReadkafkaTest {
+object WordCountExample {
 
   def main(args: Array[String]): Unit = {
 
@@ -18,12 +17,16 @@ object ReadkafkaTest {
     prop.setProperty("group.id", "testing-abs")
     val source = Utils.getKafkaSource[String]("09", new SimpleStringSchema(), "test", prop)
 
+
     val stream = env.addSource(source)
+         val st = stream
+           .flatMap(x=>x.split("\\W+")) // split by space
+           .map(x=>(x,1))    // map to  word ==> word ,1
+           .keyBy(0)   // group by word
+           .sum(1)   // sum ->value
 
+    st.print()
 
-
-    stream.print()
-
-     env.execute()
+    env.execute()
   }
 }
